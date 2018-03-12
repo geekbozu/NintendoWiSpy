a0dbd17a1721084c77b72671955cf29b924ca06f
geekboy1011 <geekboy1011@gmail.com>
2018-03-12 18:45:41 -0400

    Added Some optimizations and code cleanup

--- NintendoWiSpy.js
+++ NintendoWiSpy.js
@@ -1,11 +1,14 @@
-var heartbeatMsg = '--heartbeat--', heartbeatInterval = null, missedHeartbeats = 0;
 window.onload = function(){
     var theme = 'theme\\' + getAllUrlParams().theme + '\\',
         config = JSON.parse(getJSON(theme + 'config.json')),
         canvas = document.getElementById('myCanvas'),
         ctx = canvas.getContext('2d'),
-        i;
-        
+        heartbeatMsg = '--heartbeat--',
+        heartbeatInterval = null,
+        missedHeartbeats = 0,
+        i,
+        socket = new ReconnectingWebsocket('ws://' + getAllUrlParams().websocketserver + ':' + getAllUrlParams().websockport);
+
     if(config.width){
         canvas.width = config.width;
     }
@@ -15,7 +18,6 @@ window.onload = function(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.textAlign = "center";
     ctx.fillText('Waiting for connection', canvas.width / 2, canvas.height / 2);
-    var socket = new ReconnectingWebsocket('ws://' + getAllUrlParams().websocketserver + ':' + getAllUrlParams().websockport);
 
     socket.onopen = function(event){
         console.log('Connected to: ' + event.currentTarget.url);
@@ -32,8 +34,8 @@ window.onload = function(){
                     clearInterval(heartbeatInterval);
                     heartbeatInterval = null;
                     ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    ctx.textAlign = "center";
-                    ctx.fillText("Waiting for connection", canvas.width/2, canvas.height/2); 
+                    ctx.textAlign = 'center';
+                    ctx.fillText('Waiting for connection', canvas.width/2, canvas.height/2);
                     console.warn('Closing connection. Reason: ' + e.message);
                     socket.close();
                 }
@@ -153,9 +155,6 @@ window.onload = function(){
                             swidth = controlsObj.analog[config.analog[i].axis] * config.analog[i].img.width;
                             break;
                     }
-                    // config.analog[i].axis
-                    // controlObj.analog[i]
-                    // context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
                     ctx.drawImage(config.analog[i].img, sx, sy, swidth, sheight, x, y, swidth, sheight);
                 }
                 break;
@@ -164,65 +163,65 @@ window.onload = function(){
         }
     };
     function extractControls(data){
-        // This is a horridly optimized funtion...
-        var ret = {};
-        ret.button = {};
-        ret.analog = {};
+        var button = {},
+            analog = {};
         // Eeems says do this https://www.irccloud.com/pastebin/zvPElZLJ/
         switch(config.controllerType){
             case'GCN':
-                ret.button.START = Boolean(~~data[3]);
-                ret.button.A = Boolean(~~data[7]);
-                ret.button.B = Boolean(~~data[6]);
-                ret.button.X = Boolean(~~data[5]);
-                ret.button.Y = Boolean(~~data[4]);
-                ret.button.Z = Boolean(~~data[11]);
-                ret.button.L = Boolean(~~data[9]);
-                ret.button.R = Boolean(~~data[10]);
+                button.START = Boolean(~~data[3]);
+                button.A = Boolean(~~data[7]);
+                button.B = Boolean(~~data[6]);
+                button.X = Boolean(~~data[5]);
+                button.Y = Boolean(~~data[4]);
+                button.Z = Boolean(~~data[11]);
+                button.L = Boolean(~~data[9]);
+                button.R = Boolean(~~data[10]);
                 // Todo analog shennangins
 
-                ret.analog.joyStickX = (getMultiByte(data, 16) - 128) / 128;
-                ret.analog.joyStickY = (getMultiByte(data, 16 + 8) - 128) / 128;
-                ret.analog.cStickX = (getMultiByte(data, 16 + 16) - 128) / 128;
-                ret.analog.cStickY = (getMultiByte(data, 16 + 24) - 128) / 128;
-                ret.analog.lTrig = getMultiByte(data, 16 + 32) / 256;
-                ret.analog.rTrig = getMultiByte(data, 16 + 40) / 256;
+                analog.joyStickX = (getMultiByte(data, 16) - 128) / 128;
+                analog.joyStickY = (getMultiByte(data, 16 + 8) - 128) / 128;
+                analog.cStickX = (getMultiByte(data, 16 + 16) - 128) / 128;
+                analog.cStickY = (getMultiByte(data, 16 + 24) - 128) / 128;
+                analog.lTrig = getMultiByte(data, 16 + 32) / 256;
+                analog.rTrig = getMultiByte(data, 16 + 40) / 256;
                 break;
 
             case'N64':
-                ret.button.START = Boolean(~~data[3]);
-                ret.button.A = Boolean(~~data[0]);
-                ret.button.B = Boolean(~~data[1]);
-                ret.button.Z = Boolean(~~data[2]);
-                ret.button.L = Boolean(~~data[10]);
-                ret.button.R = Boolean(~~data[11]);
-                ret.button.up = Boolean(~~data[4]);
-                ret.button.down = Boolean(~~data[5]);
-                ret.button.left = Boolean(~~data[6]);
-                ret.button.right = Boolean(~~data[7]);
-                ret.button.Cup = Boolean(~~data[12]);
-                ret.button.Cdown = Boolean(~~data[13]);
-                ret.button.Cleft = Boolean(~~data[14]);
-                ret.button.Cright = Boolean(~~data[15]);
+                button.START = Boolean(~~data[3]);
+                button.A = Boolean(~~data[0]);
+                button.B = Boolean(~~data[1]);
+                button.Z = Boolean(~~data[2]);
+                button.L = Boolean(~~data[10]);
+                button.R = Boolean(~~data[11]);
+                button.up = Boolean(~~data[4]);
+                button.down = Boolean(~~data[5]);
+                button.left = Boolean(~~data[6]);
+                button.right = Boolean(~~data[7]);
+                button.Cup = Boolean(~~data[12]);
+                button.Cdown = Boolean(~~data[13]);
+                button.Cleft = Boolean(~~data[14]);
+                button.Cright = Boolean(~~data[15]);
 
-                ret.analog.joyStickX = (~~getMultiByte(data, 16));
-                ret.analog.joyStickY = (~~getMultiByte(data, 16 + 8));
-                if(ret.analog.joyStickX > 127){
-                    ret.analog.joyStickX -= 256;
+                analog.joyStickX = (~~getMultiByte(data, 16));
+                analog.joyStickY = (~~getMultiByte(data, 16 + 8));
+                if(analog.joyStickX > 127){
+                    analog.joyStickX -= 256;
                 }
-                if(ret.analog.joyStickY > 127){
-                    ret.analog.joyStickY -= 256;
+                if(analog.joyStickY > 127){
+                    analog.joyStickY -= 256;
                 }
-                ret.analog.joyStickX /= 128;
-                ret.analog.joyStickY /= 128;
+                analog.joyStickX /= 128;
+                analog.joyStickY /= 128;
                 break;
             default:
                 console.log('No Controller type');
         }
-        return ret;
+        return {
+            button: button,
+            analog: analog
+        };
     }
-};
-/*            byte val = 0;
+    /*            byte val = 0;
             for (int i = 0 ; i < 8 ; ++i) {
                 if ((packet[i+offset] & 0x0F) != 0) {
                     val |= (byte)(1<<(7-i));
@@ -230,94 +229,95 @@ window.onload = function(){
             }
             ret
 */
-function getMultiByte(data, offset){
-    var val, j = 0;
-    for(j = 0; j < 8; ++j){
-        if(data[j + offset] == '1'){
-            val |= 1 << (7 - j);
+    function getMultiByte(data, offset){
+        var val, j = 0;
+        for(j = 0; j < 8; ++j){
+            if(data[j + offset] == '1'){
+                val |= 1 << (7 - j);
+            }
         }
+        return val;
     }
-    return val;
-}
-// Graciously stolen from sitepoint 
-// https://www.sitepoint.com/get-url-parameters-with-javascript/
-function getAllUrlParams(url){ 
+    // Graciously stolen from sitepoint
+    // https://www.sitepoint.com/get-url-parameters-with-javascript/
+    function getAllUrlParams(url){
 
-    // get query string from url (optional) or window
-    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+        // get query string from url (optional) or window
+        var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
 
-    // we'll store the parameters here
-    var obj = {};
+        // we'll store the parameters here
+        var obj = {};
 
-    // if query string exists
-    if(queryString){
-        // stuff after # is not part of query string, so get rid of it
-        queryString = queryString.split('#')[0];
+        // if query string exists
+        if(queryString){
+            // stuff after # is not part of query string, so get rid of it
+            queryString = queryString.split('#')[0];
 
-        // split our query string into its component parts
-        var arr = queryString.split('&');
+            // split our query string into its component parts
+            var arr = queryString.split('&');
 
-        for(var i = 0; i < arr.length; i++){
-            // separate the keys and the values
-            var a = arr[i].split('=');
+            for(var i = 0; i < arr.length; i++){
+                // separate the keys and the values
+                var a = arr[i].split('=');
 
-            // in case params look like: list[]=thing1&list[]=thing2
-            var paramNum;
-            var paramName = a[0].replace(/\[\d*\]/, function(v){
-                paramNum = v.slice(1, -1);
-                return '';
-            });
+                // in case params look like: list[]=thing1&list[]=thing2
+                var paramNum;
+                var paramName = a[0].replace(/\[\d*\]/, function(v){
+                    paramNum = v.slice(1, -1);
+                    return '';
+                });
 
-            // set parameter value (use 'true' if empty)
-            var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+                // set parameter value (use 'true' if empty)
+                var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
 
-            // (optional) keep case consistent
-            paramName = paramName.toLowerCase();
-            paramValue = paramValue.toLowerCase();
+                // (optional) keep case consistent
+                paramName = paramName.toLowerCase();
+                paramValue = paramValue.toLowerCase();
 
-            // if parameter name already exists
-            if(obj[paramName]){
-                // convert value to array (if still string)
-                if(typeof obj[paramName] === 'string'){
-                    obj[paramName] = [obj[paramName]];
+                // if parameter name already exists
+                if(obj[paramName]){
+                    // convert value to array (if still string)
+                    if(typeof obj[paramName] === 'string'){
+                        obj[paramName] = [obj[paramName]];
+                    }
+                    // if no array index number specified...
+                    if(typeof paramNum === 'undefined'){
+                        // put the value on the end of the array
+                        obj[paramName].push(paramValue);
+                    }
+                    // if array index number specified...
+                    else{
+                    // put the value at that index number
+                        obj[paramName][paramNum] = paramValue;
+                    }
                 }
-                // if no array index number specified...
-                if(typeof paramNum === 'undefined'){
-                    // put the value on the end of the array
-                    obj[paramName].push(paramValue);
-                }
-                // if array index number specified...
+                // if param name doesn't exist yet, set it
                 else{
-                // put the value at that index number
-                    obj[paramName][paramNum] = paramValue;
+                    obj[paramName] = paramValue;
                 }
             }
-            // if param name doesn't exist yet, set it
-            else{
-                obj[paramName] = paramValue;
-            }
         }
+
+        return obj;
     }
 
-    return obj;
-}
+    // Stack overflow to the rescue
+    // needs replacment eventually "deprecated" Bah
+    function getJSON(url){
+        var resp, xmlHttp;
+        resp = '';
+        xmlHttp = new XMLHttpRequest();
 
-// Stack overflow to the rescue
-// needs replacment eventually "deprecated" Bah
-function getJSON(url){
-    var resp, xmlHttp;
-    resp = '';
-    xmlHttp = new XMLHttpRequest();
+        if(xmlHttp != null){
+            xmlHttp.open('GET', url, false);
+            xmlHttp.send(null);
+            resp = xmlHttp.responseText;
+        }
 
-    if(xmlHttp != null){
-        xmlHttp.open('GET', url, false);
-        xmlHttp.send(null);
-        resp = xmlHttp.responseText;
-    }
-
-    return resp;
-};
-function noop() {}
-function heartbeat(){
-    this.isAlive = true;
+        return resp;
+    };
+    function noop(){}
+    function heartbeat(){
+        this.isAlive = true;
+    };
 };
